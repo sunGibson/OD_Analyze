465c27b31abeabea16a18f6c08e847dd09aefc5e
sunGibson <13465816527@163.com>
2018-04-12 17:08:02 +0800

    OD_car_analyze出租车OD分析

---
+++ src/main/scala/com/triton/aio/OD_car_analyze.scala
@@ -0,0 +1,28 @@
+package com.triton.aio
+
+import org.apache.spark.sql._
+
+/**
+  * Created by cuitu on 2018/4/10.
+  */
+object OD_car_analyze {
+  def main(args: Array[String]): Unit = {
+    val CAR_GPS_FilePath = "hdfs://master:9000/user/yuty/data/"
+    val CAR_GPS_FileName = "all_gps_1603"
+
+    val spark = SparkSession.builder().master("spark://master:7077").appName("OD_car_analyze").getOrCreate()
+    val ORIGINAL_DATA = spark.read.format("csv").option("delimiter","|").load(CAR_GPS_FilePath+CAR_GPS_FileName)
+    //读取数据
+    val GPS_DATA = ORIGINAL_DATA.selectExpr("_c0 CARID","CAST(_c3 AS INT) EMPTY","CAST(_c9 as timestamp) TIME","CAST(_c10 as DOUBLE) LONGITUDE","CAST(_c11 AS DOUBLE) LATITUDE","CAST(_c12 AS DOUBLE) SPEED")
+    GPS_DATA.filter(GPS_DATA("EMPTY") === 0 || GPS_DATA("EMPTY") === 1).createOrReplaceTempView("TB1")
+    //过滤无效数据
+    spark.sql("SELECT *,row_number() over(PARTITION BY CARID ORDER BY CARID,TIME) N FROM TB1").createOrReplaceTempView("TB2")
+    spark.sql("SELECT CARID MIDID,EMPTY MID_EMPTY,N M FROM TB2 WHERE N > 0").createOrReplaceTempView("TB3")
+    val OD_CAR_DATA = spark.sql("SELECT A.*,B.* FROM TB2 A JOIN TB3 B ON  A.N+1 = B.M AND A.CARID = B.MIDID AND EMPTY <> MID_EMPTY ORDER BY CARID,TIME")
+    //获取OD点数据
+    OD_CAR_DATA.write.mode(SaveMode.Overwrite).format("csv").option("header","true").save(CAR_GPS_FilePath + "output/OD_CAR_DATA")
+
+  }
+
+
+}
--- src/main/scala/com/triton/aio/OD_trip_analyze.scala
+++ src/main/scala/com/triton/aio/OD_trip_analyze.scala
@@ -1,49 +1,51 @@
-package main.scala.com.triton.aio
+package com.triton.aio
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 
 /**
-  * Created by cuitu on 2018/4/9.
+  * Created by cuitu on 2018/3/26.
   */
-object OD_department_analyze {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().master("spark://master:7077").appName("OD_Analyze").getOrCreate()
-    //读取GSM数据，并转换数据格式
-    val GSM20140303MD = spark.read.format("csv").option("header", "true").option("inferSchema", "false").load("hdfs://master:9000/user/yuty/data/GSM20140303MD0.csv")
-    //做数据转换
+object OD_trip_analyze {
+  def main(args: Array[String]) {
+    val GSMFilePath = "hdfs://master:9000/user/yuty/data/"
+    val GSMFileName = "GSM20140303MD.csv"
+    val GSMStationName = "BasestationSArea2014.csv"
 
-    val ts = unix_timestamp(GSM20140303MD("FDT_TIME"), "yyyy/MM/dd HH:mm:ss").cast("timestamp")
-    val GSM20140303MDa = GSM20140303MD.selectExpr("FSTR_MSID MSID", "cast(FFLT_X as float) X", "cast(FFLT_Y as float) Y", "FDT_TIME").withColumn("TIMESTAMP", ts)
-    GSM20140303MDa.createOrReplaceTempView("GSM20140303MDaTable")
-    //读取研究区域基站数据
-    val basestationA = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("hdfs://master:9000/user/yuty/data/BasestationSArea2014.csv")
-    val basestationA1 = basestationA.selectExpr("cast(X as float) X", "cast(Y as float) Y", " Area")
-    basestationA1.createOrReplaceTempView("basestationATable")
+    //初始化sparkSession
+    val spark = SparkSession.builder().master("spark://master:7077").appName("OD_trip_analyze").getOrCreate()
+    //读取GSM数据
+    val GSMDATA = spark.read.format("csv").option("header", true).option("inferSchema", false).load(GSMFilePath + GSMFileName)
+    //时间格式转换
+    val TS = unix_timestamp(GSMDATA("FDT_TIME"), "yyyy/MM/dd HH:mm:ss").cast("timestamp")
+    GSMDATA.selectExpr("FSTR_MSID MSID", "CAST(FFLT_X AS FLOAT) X", "CAST(FFLT_Y AS FLOAT) Y", "FDT_TIME").withColumn("TIMESTAMP", TS).createOrReplaceTempView("GSMDATA_TABLE_0")
+    //创建[MSID(string),X(float),Y(float),FDT_TIMR(string),TS(timestamp)]表,格式化GSM数据
 
-    //计算用户夜间居住地
-    val GSM20140303MDb = spark.sql("select * from GSM20140303MDaTable where TIMESTAMP between '2014-03-03 00:00:00' and '2014-03-03 06:00:00' ")
-    GSM20140303MDb.createOrReplaceTempView("GSM20140303MDbTable")
+    val GSMDATA_1 = spark.read.format("csv").option("header", true).option("inferSchema", false).load(GSMFilePath + GSMStationName)
+    GSMDATA_1.selectExpr("CAST(X AS FLOAT) X", "CAST(Y AS FLOAT) Y", "Area").createOrReplaceTempView("GSMDATA_TABLE_1")
+    //创建[X(float),Y(float),Area(string)]表，格式化小区数据
 
-    //根据用户ID、经纬度分组统计
-    val GSM20140303MDc = spark.sql("select MSID,X,Y,count(*) N from GSM20140303MDbTable group by MSID,X,Y")
-    GSM20140303MDc.createOrReplaceTempView("GSM20140303MDcTable")
+    spark.sql("SELECT A.MSID AS MSID,A.TIMESTAMP,A.X,A.Y,B.Area FROM GSMDATA_TABLE_0 A INNER JOIN GSMDATA_TABLE_1 B on A.X=B.X AND A.Y=B.Y ORDER BY MSID,TIMESTAMP").filter("TIMESTAMP is not null").createOrReplaceTempView("GSMDATA_TABLE_2")
+    //创建[MSID(string),TIMESTAMP(timestamp),X(float),Y(float),Area(string)]表并过滤空值,匹配GSM数据到小区按照用户时间排序
+    spark.sql("SELECT MSID,TIMESTAMP,MAX(X) X,MAX(Y) Y,MAX(Area) Area FROM GSMDATA_TABLE_2 GROUP BY MSID,TIMESTAMP").createOrReplaceTempView("GSMDATA_TABLE_3")
+    //将GSMDATA_TABLE_2表中的重复时间数据过滤
+    spark.sql("SELECT * FROM (SELECT *,row_number() over (PARTITION BY MSID,Area ORDER BY MSID,TIMESTAMP DESC) AS num FROM GSMDATA_TABLE_3) A WHERE A.num=1 UNION SELECT * FROM (SELECT *,row_number() over (PARTITION BY MSID,Area ORDER BY MSID,TIMESTAMP) AS num FROM GSMDATA_TABLE_3) B WHERE B.num=1").createOrReplaceTempView("GSMDATA_TABLE_4")
+    //去除GSMDATA_TABLE_3表的中间点
+    spark.sql("select MSID, TIMESTAMP, X, Y,Area from GSMDATA_TABLE_4 ORDER BY MSID,TIMESTAMP").createOrReplaceTempView("GSMDATA_TABLE_5")
+    //对GSMDATA_TABLE_4表的数据按照MSID,TIMESTAMP进行排序
+    spark.sql("SELECT MSID FROM GSMDATA_TABLE_5 GROUP BY MSID HAVING  count(MSID) > 1")createOrReplaceTempView("GSMDATA_TABLE_6")
+    //挑选出符合的数据MSID（sparksql 不支持in，exsits等操作）
+    spark.sql("SELECT MSID,TIMESTAMP,X,Y,Area FROM GSMDATA_TABLE_5 A LEFT SEMI JOIN GSMDATA_TABLE_6 B ON (A.MSID=B.MSID)").createOrReplaceTempView("GSMDATA_TABLE_7")
+    //挑选出GSMDATA_TABLE_5中符合条件的数据
+    spark.sql("SELECT MSID,X OX,Y OY,Area OA,TIMESTAMP ENTERTIME,row_number() over(PARTITION BY MSID ORDER BY MSID,TIMESTAMP) N FROM GSMDATA_TABLE_7").createOrReplaceTempView("GSMDATA_TABLE_8")
+    //对GSMDATA_TABLE_7进行分组排序并添加N(自增)
+    spark.sql("SELECT ENTERTIME DEPARTTIME,OX DX,OY DY,OA DA,N,MSID ID FROM GSMDATA_TABLE_8 WHERE N > 0").createOrReplaceTempView("GSMDATA_TABLE_9")
+    spark.sql("SELECT A.MSID,A.OX,A.OY,A.OA,A.ENTERTIME,B.DX,B.DY,B.DA,B.DEPARTTIME,A.N,B.ID FROM GSMDATA_TABLE_8 A JOIN GSMDATA_TABLE_9 B ON A.N+1=B.N AND A.MSID=B.ID").createOrReplaceTempView("GSMDATA_TABLE_10")
+    spark.sql("SELECT * FROM GSMDATA_TABLE_10 WHERE (cast(DEPARTTIME AS long)-cast(ENTERTIME AS long))>1800").createOrReplaceTempView("GSMDATA_TABLE_11")
+    val OD_ANALYZE = spark.sql("SELECT OA,DA,count(*) ALLOD FROM GSMDATA_TABLE_11 GROUP BY OA,DA")
+    OD_ANALYZE.write.mode(SaveMode.Overwrite).format("csv").option("header", "true").save(GSMFilePath+"output/ALL_OD_ANALYZE")
+    //最终的OD出行
+    spark.close()
 
-    //对数据进行分组、排序统计
-    val GSM20140303MDd1 = spark.sql("select *,row_number() over (PARTITION BY MSID order by N Desc) as ID from GSM20140303MDcTable")
-    GSM20140303MDd1.createOrReplaceTempView("GSM20140303MDd1Table")
-
-    //获取每个用户占比最多经纬度的数据
-    val GSM20140303MDd2 = spark.sql("select MSID,X,Y,N from GSM20140303MDd1Table where ID=1")
-    GSM20140303MDd2.createOrReplaceTempView("GSM20140303MDdTable")
-
-    //筛选研究区域的有效用户、过滤不能与区域经纬度匹配的点
-    val GSM20140303MDe = spark.sql("select A.MSID as MSID,A.X,A.Y,B.Area FROM GSM20140303MDdTable A inner join basestationATable B on A.X=B.X AND A.Y=B.Y")
-    GSM20140303MDe.createOrReplaceTempView("GSM20140303MDeTable")
-
-    //0)输出用户居住地数据
-    GSM20140303MDe.coalesce(1).write.mode(SaveMode.Overwrite).format("csv").option("header", "true").save("/user/sunjp/data/User20140303AllS.csv")
-    GSM20140303MDe.show(false) // coalesce(1).   emerged all file to one
-    spark.stop()
   }
 }
